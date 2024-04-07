43e4ddb34c002a497f9a2b44be7e005ce67d5f84
TingChiehLin <cooloojayoo@gmail.com>
2024-04-07 12:06:09 +1000

    Add - add a new key into dictionary

--- main.py
+++ main.py
@@ -5,5 +5,9 @@ points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 
 
 letter_to_points = {key:value for key, value in zip(letters, points)}
 
-print(letter_to_points)
+print("Using a list comprehension and zip, create a dictionary called letter_to_points that has the elements of letters as the keys and the elements of points as the values.",letter_to_points)
+
+letter_to_points[" "] = 0
+
+print("letter_to_points dictionary that has a key of " " and a point value of 0",letter_to_points)
 
