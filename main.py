dab93086646e11fb7bd76ca07c9857adbe34532f
TingChiehLin <cooloojayoo@gmail.com>
2024-04-07 19:29:35 +1000

    Done - practice

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ print("====Scrabble Project====")
 letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
 
-letter_to_points = {key:value for key, value in zip(letters, points)}
+letter_to_points = {
+  key: value 
+  for key, value 
+  in zip(letters, points)
+}
 
 print("Using a list comprehension and zip, create a dictionary called letter_to_points that has the elements of letters as the keys and the elements of points as the values.",letter_to_points)
 
@@ -11,3 +15,36 @@ letter_to_points[" "] = 0
 
 print("letter_to_points dictionary that has a key of " " and a point value of 0",letter_to_points)
 
+print("return how many points that word is worth. Define a function called score_word that takes in a parameter word")
+def score_word(word):  
+  print_total = 0
+  for letter in word:
+    print_total += letter_to_points.get(letter,0)
+  return print_total
+
+brownie_points = score_word("BROWNIE")
+
+print(brownie_points)
+
+player_to_words = {
+  "player1":["BLUE","TENNIS","EXIT"],
+  "wordNerd":["EARTH","EYES","MACHINE"],
+  "Lexi Con":["ERASER","BELLY","HUSKY"],
+  "Prof Reader":["ZAP","COMA","PERIOD"],
+}
+
+player_to_points = {}
+for player, words in player_to_words.items():
+  player_points = 0
+  for word in words:
+    player_points += score_word(word)
+player_to_points[player] = player_points
+
+print("player_to_points :",player_to_points)
+
+def play_word(player, word):
+  player_to_words[player].append(word)
+
+play_word("player1","CODE")
+
+print("player_to_words: ",player_to_words)
